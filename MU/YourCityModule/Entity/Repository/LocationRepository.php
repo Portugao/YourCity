e268c6014ccdb0a03e69b70b7ebf83037a5268eb
Portugao <info@homepages-mit-zikula.de>
2017-05-02 23:26:36 +0200

    new field for ordering

--- MU/YourCityModule/Entity/Repository/LocationRepository.php
+++ MU/YourCityModule/Entity/Repository/LocationRepository.php
@@ -21,5 +21,8 @@ use MU\YourCityModule\Entity\Repository\Base\AbstractLocationRepository;
  */
 class LocationRepository extends AbstractLocationRepository
 {
-    // feel free to add your own methods here, like for example reusable DQL queries
+    /**
+     * @var string The default sorting field/expression
+     */
+    protected $defaultSortingField = 'letterForOrder';
 }
