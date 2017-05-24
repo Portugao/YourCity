dc171021f4a4e7ad6753b4cb6aaf06b1e1f3b2f8
Portugao <info@homepages-mit-zikula.de>
2017-05-25 00:17:19 +0200

    check for user uid

--- MU/YourCityModule/Helper/ListEntriesHelper.php
+++ MU/YourCityModule/Helper/ListEntriesHelper.php
@@ -157,8 +157,8 @@ class ListEntriesHelper extends AbstractListEntriesHelper
      */
     public function getLocations()
     {
-    	//$uid = $this->controllerHelper->getUid();
-    	$uid = 2;
+    	$uid = $this->controllerHelper->getUid();
+    	//$uid = 2;
     	$locationRepository = $this->entityFactory->getRepository('location');
     	if ($uid != 2) {
     	$criteria = array('owner' => $uid);
