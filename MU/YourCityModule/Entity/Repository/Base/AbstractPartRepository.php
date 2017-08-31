725075eb3ceefb80191d2c7ad72f0f21afa8f15c
Portugao <info@homepages-mit-zikula.de>
2017-08-31 21:25:51 +0200

    Repository update

--- MU/YourCityModule/Entity/Repository/Base/AbstractPartRepository.php
+++ MU/YourCityModule/Entity/Repository/Base/AbstractPartRepository.php
@@ -61,6 +61,7 @@ abstract class AbstractPartRepository extends EntityRepository
     {
         return [
             'name',
+            'imageOfPart',
             'createdBy',
             'createdDate',
             'updatedBy',
