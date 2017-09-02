2a465df288c8b6c8d05494eac5272b5d9f0f22fb
Portugao <info@homepages-mit-zikula.de>
2017-09-02 23:25:55 +0200

    own icon

--- MU/YourCityModule/Resources/public/js/MUYourCityModule.Geo.js
+++ MU/YourCityModule/Resources/public/js/MUYourCityModule.Geo.js
@@ -21,10 +21,22 @@ function mUYourCityInitGeographicalDisplay(parameters, isEditMode)
     L.tileLayer(parameters.tileLayerUrl, {
         attribution: parameters.tileLayerAttribution
     }).addTo(map);
+    
+    var achseIcon = L.icon({
+        iconUrl: '../../modules/MU/YourCityModule/Resources/public/images/Logo_Marker.png',
+        //shadowUrl: 'leaf-shadow.png',
+
+        iconSize:     [40, 40], // size of the icon
+        //shadowSize:   [50, 64], // size of the shadow
+        iconAnchor:   [20, 30], // point of the icon which will correspond to marker's location
+        //shadowAnchor: [4, 62],  // the same for the shadow
+        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    });
 
     // add a marker
     marker = new L.marker(centerLocation, {
-        draggable: isEditMode
+        draggable: isEditMode,
+        icon: achseIcon
     });
     marker.addTo(map);
 
